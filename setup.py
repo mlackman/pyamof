bd35a8ebeea9fec56a985285f3caa035b294c0b6
Mika.Lackman <Mika.Lackman@55daa3fe-6330-11de-b47b-210fc227597e>
2010-08-09 06:16:46 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 
+DESCRIPTION = 'A Python (yet another) mock framework'
+
 setup(name='yamf',
       version='0.6',
       py_modules=['yamf'],
+      author='Mika Lackman',
+      author_email='mika.lackman@gmail.com',
+      description=DESCRIPTION,
       )
