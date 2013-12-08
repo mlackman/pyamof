c80c6d30069cba8b21b6aae071419128eeabd7f3
mika.lackman@gmail.com <mika.lackman@gmail.com@55daa3fe-6330-11de-b47b-210fc227597e>
2013-12-08 06:18:16 +0000

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='yamf',
-      version='0.6',
+      version='0.7',
       py_modules=['yamf'],
       author='Mika Lackman',
       author_email='mika.lackman@gmail.com',
@@ -9,7 +9,7 @@ setup(name='yamf',
       platforms=['OS independent'],
       description='A Python (yet another) mock framework',
       url = 'http://code.google.com/p/pyamof/',
-      download_url = 'http://code.google.com/p/pyamof/downloads/detail?name=yamf-0.6.tar.gz&can=2&q=',
+      download_url = 'http://code.google.com/p/pyamof/downloads/detail?name=yamf-0.7.tar.gz&can=2&q=',
       keywords = ['mock', 'tdd'],
       classifiers = [
         'Programming Language :: Python',
